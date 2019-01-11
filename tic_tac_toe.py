1a18da40272a619463d07b0ca6cd512b2c097966
juneja2 <38742950+juneja2@users.noreply.github.com>
2019-01-10 22:31:08 -0800

    Update tic_tac_toe.py
    Slightly less code, but much more readable since I got rid of global variables. I tested this quite a lot. If there is a bug let me know.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,158 +1,135 @@
-
 import os
 
-player1 = None
-player2 = None
-times = 1
-player1_turn = True
-input_list = [" "] * 10
+def set_player2(player1):
+    return "O" if player1 == "X" else "X"
+
+def get_players():
+    player1 = None
+    while True:
+        player1 = input("Player 1: Do you want to be X or O ").upper()
+        if player1 in "OX":
+            break
+        else:
+            print("Invalid symbol for player1")
+        
+    player2 = set_player2(player1)
+    return player1, player2
 
 def wanna_play():
-    global player1
-    global player2
-    global times
-
-    if times == 1:
-        print("Welcome to Tic Tac Toe")
-        player1 = ask_X_or_O_for_player1()
-        player2 = set_player2(player1)
-    else:
-        switch_player_position()
-         
-    times += 1
-    
-    return valid_input()
-
-def ask_X_or_O_for_player1():
+    choice = None
     while True:
-        player1 = input("Player 1: Do you want to be X or O\n").upper()
-        if player1 in "OX":
-            return player1
-
-def set_player2(player1):
-    if player1 == "X":
-        return "O"
-
-    return "X"
-
-def switch_player_position():
-    global player1
-    global player2
-    if player1 == "X":
-        player1 = "O"
-        player2 = "X"
-    else:
-        player1 = "X"
-        player2 = "O"
-
-def valid_input():
-    while True:
-        choice = input("Are you ready to play. Enter Yes or No\n").lower()
+        choice = input("Do you want to play? Enter Yes or No ").lower()
         if choice == "yes":
-            print("Player 1 will go first")
             return True
         elif choice == "no":
             return False
         else:
-            print("Please enter Yes or No")
-
-def change_player_turn():
-    global player1_turn
-    if player1_turn:
-        player1_turn = False
-    else:
-        player1_turn = True
-
-def valid_input_pos():
+            print("Invalid answer")
     
-    global input_list
-    while True:
-        input_pos = int(input("Choose your next position 1-9 "))
-        if  input_pos in range(1, 10):
-            if input_list[input_pos] != " ":
-                print("Invalid position specified")
-            elif player1_turn:
-                input_list[input_pos] = player1
-                return
-            else:
-                input_list[input_pos] = player2
-                return
-        else:
-            print("Out of range position")
+def display_board(input_list):
+    components = {"empty": "     |     |     \n", "border":"-----------------\n", 0: "  {}  |  {}  |  {}  \n"}
 
-def display_board():
+    for i in range(7, 0, -3):
+        print(components["empty"])
+        print(components[0].format(input_list[i], input_list[i + 1], input_list[i + 2]))
+        print(components["empty"])
 
-    component = {"empty": "     |     |     \n", "border":"-----------------\n", 0: "  {}  |  {}  |  {}  \n"}
-
-    # Inserted " " in the place where there isn't a number
-    
-    #Now printing the tic tac toe with X and O's
-    #In the format 
-    # 7 8 9
-    # 4 5 6
-    # 1 2 3
-    i = 7
-    while i >= 1:
-        print(component["empty"])
-        print(component[0].format(input_list[i], input_list[i + 1], input_list[i + 2]))
-        print(component["empty"])
-        
         if i != 1:
-            print(component["border"])
-            
-        i -= 3
-        
-def reset_input_list():
-    global input_list
-    
-    input_list = [" "] * 10
+            print(components["border"])
+def get_num_input(input_list, player1, player2, player1_turn):
+    while True:
+        num = int(input("Please enter a number between 1-9 "))
+        if num in range(1, 10):
+            if input_list[num] == " ":
+                input_list[num] = player1 if player1_turn else player2
+                return input_list
+            else:
+                print("Position already filled")
+        else:
+            print("Number out of range")
 
-def win():
-    if not player1_turn:
-        for i in range(1, 10, 3):
-            if input_list[i] == input_list[i+1] == input_list[i + 2] == player1:
-                return "p1"
-        if input_list[1] == input_list[5] == input_list[9] == player1 or input_list[3] == input_list[5] == input_list[7] == player1:
-            return "p1"
-        return False
+def change_player_turn(player1_turn):
+    return not player1_turn
+
+def switch_player_symbols(player1):
+    if player1 == "X":
+        return ("O", "X")
+    return ("X", "O")
+def someone_won(input_list, player1, player2):
+
+    for i in range(0, 3):
+        j = 3 * i
+        equal1 = input_list[j + 1] == input_list[j + 2] == input_list[j + 3]
+        equal2 = input_list[i + 1] == input_list[i + 4] == input_list[i + 7]
         
-    else:
-        for i in range(1, 10, 3):
-            if input_list[i] == input_list[i+1] == input_list[i + 2] == player2:
-                return "p2"
-        if input_list[1] == input_list[5] == input_list[9] == player2 or input_list[3] == input_list[5] == input_list[7] == player2:
-            return "p2"
-        return False
+        if equal1:
+            if input_list[j + 1] == player1:
+                return player1
+            elif input_list[j + 1] == player2:
+                return player2
+        elif equal2:
+            if input_list[i + 1] == player1:
+                return player1
+            elif input_list[i + 1] == player2:
+                return player2
+
+        
+    first_check = input_list[1] == input_list[5] == input_list[9]
+    second_check = input_list[3] == input_list[5] == input_list[7]
+
+    if first_check or second_check:
+        if input_list[5] == player1:
+            return player1
+        elif input_list[5] == player2:
+            return player2  
+
+    return False
+        # Returning False if nobody wons
+        # Returning player if somebody wins
 
 def main_loop():
-    total_inputs = 0
+    input_list = [" "] * 10
+    player1 = None
+    player2 = None
+    player1_turn = True
+    
+    print("Welcome to Tic Tac Toe")
+    player1, player2 = get_players()
+
     while wanna_play():
-        # WE need to keep track of the places where what has gone
         
-        while total_inputs < 10:
-            w = win()
-            if type(w) != bool or total_inputs == 9:
-                os.system("cls")
-                break
-            else:
-                os.system("cls")
-                
-                display_board()
-                
-                valid_input_pos()
-                
-                total_inputs += 1
-                change_player_turn()
-        
-        display_board()
-        if win() == "p1":
-            print("Player 1 wins")
-        elif win() == "p2":
-            print("Player 2 wins")
-        else:
-            print("It's a tie")
-            
-        reset_input_list()
         total_inputs = 0
         
+            
+        while total_inputs < 9:
+            winner = someone_won(input_list, player1, player2)
+            os.system("cls")
+            if winner ==  False:
+                # Checking winner == False because it also possible that someone_won return a string
+                display_board(input_list)
+                # Our updated input_list and change_player_turn
+                input_list = get_num_input(input_list, player1, player2, player1_turn)
+                player1_turn = change_player_turn(player1_turn)
+
+                os.system("cls")
+                total_inputs += 1
+            else:
+                #We will only come here if we have a winner
+                break
+        winner = someone_won(input_list, player1, player2)
+        
+        display_board(input_list)
+        if winner == False:
+            #This means we have a tie
+            print("It's a tie")
+        elif winner == player1:
+            print("Player1 wins")
+        else:
+            print("Player2 wins")
+        player1, player2 = switch_player_symbols(player1)
+        input_list = [" "] * 10
+        # Resetting the input list
+        
+
+
 main_loop()
